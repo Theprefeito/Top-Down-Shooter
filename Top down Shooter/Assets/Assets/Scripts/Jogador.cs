5c6dfe816314bdb49ab7b9eb3c5021b65c9a9a5e
NotMayor <soares.saymon@escolar.ifrn.edu.br>
2025-09-09 14:33:28 -0300

    Inicio do projeto

---
+++ Top down Shooter/Assets/Assets/Scripts/Jogador.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    
+    [SerializeField] private int vidas;
+    [SerializeField] private int energia;
+    [SerializeField] private float velocidade;
+
+    // vidas
+    public void setVidas(int Vidas)
+    {
+        this.vidas = Vidas;
+    }
+
+    public int getVidas()
+    {
+        return this.vidas;
+    }
+
+    //energia
+    public void setEnergia(int energia)
+    {
+        this.energia = energia;
+    }
+
+    public int getEnergia()
+    {
+        return this.energia;
+    }
+
+    //velocidade
+    public void setVelocidade(float velocidade)
+    {
+        this.velocidade = velocidade;
+
+    }
+
+    public float getVelocidade()
+    {
+        return this.velocidade;
+    }
+    
+}
